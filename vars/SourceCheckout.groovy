2687f072f62051c0554b38510c9185c9c234250b
DEMUDU RAVI TEJA <40339126+demuduraviteja@users.noreply.github.com>
2022-08-22 17:02:28 +0530

    updated checkout line with gitcred id

--- vars/SourceCheckout.groovy
+++ vars/SourceCheckout.groovy
@@ -7,7 +7,7 @@ def call(def giturl, def branch, def git_Cred_ID)
   echo ""
   if (giturl.contains("github.com"))
   {
-    checkout([$class: 'GitSCM', branches: [[name: branch]], extensions: [], userRemoteConfigs: [[credentialsId: 'githubcredentials', url: giturl]]])
+    checkout([$class: 'GitSCM', branches: [[name: branch]], extensions: [], userRemoteConfigs: [[credentialsId: git_Cred_ID, url: giturl]]])
   }
   else
   {
